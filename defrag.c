b46d9a4525ea732bcdc0a3eeb0920babb854f9ae
ramses <rlarabel@gmail.com>
2022-12-09 23:43:50 -0500

    Still wrong but better :')

--- defrag.c
+++ defrag.c
@@ -3,20 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
+
 
 void* traverseDirectory(void *);
 
 // Shared Data
-int* frags;
+char** frags;
 size_t length = 0;
+
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
 int main(int argc, char **argv) {
 	if (argc != 3) return -1;
-
+	FILE* mp3file = fopen(argv[2], "wb");
+	fclose(mp3file);
+	mp3file = fopen(argv[2], "ab");
+	
 	struct dirent *entry;
 	chdir(argv[1]);
 	char start_dir[1000];
@@ -25,6 +31,7 @@ int main(int argc, char **argv) {
 	int num_threads = 0, err;
 	pthread_t* sub_threads = NULL;
 	char** sub_dir = NULL;
+	
 	
 	// Read all of first level sub dir (assuming no files in first level)
 	while( (entry = readdir(current)) ) {
@@ -58,16 +65,18 @@ int main(int argc, char **argv) {
 	}
 	for(int i = 0; i < num_threads; i++) free(sub_dir[i]);
 	chdir(start_dir);
-	
-	for(int i = 0; i < length+1; i++) {
-		printf("%d: %d\n", i, frags[i]);
+	if (length > 0) {
+		
+		for(int i = 0; i < length+1; i++) {
+			fwrite(frags[i], sizeof(char), strlen(frags[i]), mp3file);
+			free(frags[i]);
+		}
 	}
-	
-	printf("All threads finished\n");
 	closedir(current);
 	free(sub_dir);
 	free(sub_threads);
 	free(frags);
+	fclose(mp3file);
 
 	return 0;
 }
@@ -77,41 +86,61 @@ int main(int argc, char **argv) {
 void* traverseDirectory(void *arg) {
 	struct dirent *entry;
 	int index;
-	char cwd[1000];
+	char* buf;
+	size_t size;
 	
 
 	//Target directory
 	char* t_dir = (char *) arg;
+	
+	pthread_mutex_lock(&lock);
 	chdir(t_dir);
 	DIR* current = opendir(".");
-	char* next = NULL;
-	printf("Dir:%s\n", getcwd(cwd, sizeof(cwd)));
+	pthread_mutex_unlock(&lock);
+	
+	char* next = NULL, *file = NULL;
 	
 	if (current != NULL) {
 		while( (entry = readdir(current)) ){
-			// Finds all sub directories
-			
 			if ((strcmp(entry->d_name, ".") != 0) && (strcmp(entry->d_name, "..") != 0) && (entry->d_type == DT_DIR)) {
 				// Recursively Treverse a given directory w/ threads for each subpaths (dirs)
 				next = malloc(sizeof(char)*(strlen(t_dir)+strlen(entry->d_name)+2));
 				strcpy(next, t_dir);
 				strcat(next, "/");
-				strcat(next, entry->d_name);		
+				strcat(next, entry->d_name);
 				
 				traverseDirectory(next);
 				if(next != NULL) free(next);
 			} else if (entry->d_type == DT_REG) {
-				//printf("File:%s ...found!\n", entry->d_name);
+				// Get file exact path
+				file = malloc(sizeof(char)*(strlen(t_dir)+strlen(entry->d_name)+2));
+				strcpy(file, t_dir);
+				strcat(file, "/");
+				strcat(file, entry->d_name);
+				
+				// Open fragmenent file
+				FILE* frag_bin = fopen(file, "rb");
+				
+				// Read fragmenent file
+				fseek(frag_bin, 0, SEEK_END);
+				size = ftell(frag_bin);
+				fseek(frag_bin, 0, SEEK_SET);
+				buf = malloc(sizeof(char)*size);
+				fread(buf, sizeof(char), size, frag_bin);
+				fclose(frag_bin);
+				
+				// Write to buf to list of frag files
 				index = atoi(entry->d_name);
 				pthread_mutex_lock(&lock);
 				if(length < index) {
 					length = index;
-					frags = realloc(frags, sizeof(int)*(index + 1));
+					frags = realloc(frags, sizeof(char*)*(index + 1));
 				}
-
-				frags[index] = index;
-
+				frags[index] = strdup(buf);
+				
 				pthread_mutex_unlock(&lock);
+				free(file);
+				free(buf);
 					
 			} 
 		}
